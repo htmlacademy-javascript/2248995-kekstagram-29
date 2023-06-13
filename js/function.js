554b68a49c5078504073c8daa0c47a7d37cfd395
Sergey <asaulsergey@gmail.com>
2023-06-13 17:39:04 +0300

    Проверяет полиндром

--- js/function.js
+++ js/function.js
@@ -1,1 +1,18 @@
-function 
+// const isStringLength = (string, maxString) =>
+// 	string.length <= maxString;
+// isStringLength('Норильск', 20);
+
+const isPolindrom = string => {
+	const normalString = string
+		.replaceAll(' ', '');
+		.toUpperCase();
+let inverstedString = '';
+for (i = normalString.lenght - 1; i >= 0; i--) {
+	inverstedString = inverstedString + normalString.at(i);
+};
+return inverstedString === normalstring;
+};
+
+isPolindrom('топот');
+isPolindrom('ДовОд');
+isPolindrom('Кекс');
