03fcaf4f6fad1adf74c075477ad8094e754b718a
Sergey <asaulsergey@gmail.com>
2023-06-13 23:57:51 +0300

    fixed

--- js/function.js
+++ js/function.js
@@ -2,15 +2,15 @@
 // 	string.length <= maxString;
 // isStringLength('Норильск', 20);
 
-const isPolindrom = string => {
-	const normalString = string
-		.replaceAll(' ', '');
-		.toUpperCase();
-let inverstedString = '';
-for (i = normalString.lenght - 1; i >= 0; i--) {
-	inverstedString = inverstedString + normalString.at(i);
-};
-return inverstedString === normalstring;
+const isPolindrom = (string) => {
+  const normalString = string
+    .replaceAll(' ', '')
+    .toUpperCase();
+  let inverstedString = '';
+  for (let i = normalString.lenght - 1; i >= 0; i--) {
+    inverstedString = inverstedString + normalString.at(i);
+  }
+  return inverstedString === normalString;
 };
 
 isPolindrom('топот');
